8e3d441b90bd60798d792b00ae9b8a9a4b4843cf
PinderKingui1488 <ananimuszahar@gmail.com>
2025-04-09 14:29:10 +0300

    30.2.2

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,5 +1,6 @@
 from materials.validators import YoutubeURLValidator
 from materials.models import Course, Lesson, Subscription
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework import serializers
 
 
--- users/models.py
+++ users/models.py
@@ -1,66 +1,111 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from materials.models import Course, Lesson
+from django.forms import URLField
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта",
+    )
+    phone = models.CharField(
+        max_length=11,
+        verbose_name="Телефон",
+        null=True,
+        blank=True,
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватарка",
+        help_text="Вставьте аватарку",
+        null=True,
+        blank=True,
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        help_text="Введите город проживания",
+        null=True,
+        blank=True,
+    )
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
-
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    city = models.CharField(max_length=100, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-
-
-# materials/models.py
-from django.db import models
-from users.models import CustomUser
-
-class Course(models.Model):
-    title = models.CharField(max_length=255)
-    preview = models.ImageField(upload_to='course_previews/', blank=True)
-    description = models.TextField()
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
-        return self.title
+        return self.email
 
-class Lesson(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    preview = models.ImageField(upload_to='lesson_previews/', blank=True)
-    video_link = models.URLField()
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons') # Ссылка на курс
-
-    def __str__(self):
-        return self.title
 
 class Payments(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='payments')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_date = models.DateTimeField(auto_now_add=True)
-    course = models.ForeignKey('materials.Course', on_delete=models.SET_NULL, null=True, blank=True, related_name='payments') # Добавим связь с курсом, за который платили
-    lesson = models.ForeignKey('materials.Lesson', on_delete=models.SET_NULL, null=True, blank=True, related_name='payments') # Добавим связь с уроком, за который платили
+    PAYMENT_STATUS = [
+        ("cash", "наличные"),
+        ("transfer", "перевод на счет"),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user',
+        verbose_name="Юсер"
+    )
+    payment_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Data оплаты"
+    )
+    paid_course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='course',
+        verbose_name="Оплаченный курсик",
+        null=True, blank=True
+    )
+    separately_paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        related_name='lesson',
+        verbose_name="Оплаченный зум",
+        null=True,
+        blank=True
+    )
+    payment_amount = models.IntegerField(
+        default=0,
+        verbose_name="Сумма оплаты",
+        null=True,
+        blank=True
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_STATUS,
+        default="cash",
+        verbose_name="варианты оплаты",
+        null=True,
+        blank=True
+    )
+    link = models.URLField(
+        verbose_name="ссылка на оплаты",
+        max_length=400,
+        blank=True,
+        null=True)
+    session_id = models.CharField(
+        "ID сессии",
+        max_length=255,
+        blank=True,
+        null=True)
 
     def __str__(self):
-        return f"Payment from {self.user.email} for {self.amount}"
+        return f'{self.user} {self.paid_course}'
+
+    class Meta:
+        verbose_name = "Оплата"
+        verbose_name_plural = "Оплаты"
+
+    def __str__(self):
+        return self.session_id
